570206ac219857117a9f14134efa5550bf1368f4
aditya <2100049006ece@gmail.com>
2026-07-27 14:50:17 +0530

    first commit

---
+++ mysql-init/init.sql
@@ -0,0 +1,4 @@
+-- Initialize databases for SSMS microservices
+CREATE DATABASE IF NOT EXISTS `ssms_user_db`;
+CREATE DATABASE IF NOT EXISTS `ssms_shipment_db`;
+CREATE DATABASE IF NOT EXISTS `ssms_billing_db`;
